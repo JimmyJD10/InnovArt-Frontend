994cb3c2dffb771515297ec22b24a1c3c09b3bbb
Jimmy Ventura <jimmy.ventura@tecsup.edu.pe>
2025-07-08 18:03:03 -0500

    Actualizacion de nuevas funciones

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-export const API_URL = 'http://3.148.112.19:3001/api';
-
-// Si trabajas localmente, puedes cambiarlo a:
-// export const API_URL = 'http://localhost:3001/api';
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  (typeof window !== 'undefined' && window.location.hostname === 'localhost'
+    ? 'http://localhost:3001/api'
+    : 'http://3.148.112.19:3001/api');
